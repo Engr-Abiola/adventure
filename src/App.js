2304aa22a41a556b3919f2abb4b5769d0483be03
Engr-Abiola <abiolasuccess6@gmail.com>
2024-07-09 14:55:39 +0100

    update package.json and added basename={process.env.PUBLIC_URL} to BrowserRouter component in the App.js file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Signup  from './components/pages/Signup';
 function App() {
   return (
     < >
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
              <Navbar/> 
        <Routes> 
             <Route path="/" element ={<Home />} /> 
